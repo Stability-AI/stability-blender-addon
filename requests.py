30937bf94ce6e4b4d4da3a9dcd5c2fe8406a6e4b
DESKTOP-FP8LE75\coold <cooldude242@gmail.com>
2022-12-30 21:43:14 -0600

    cleanup

--- requests.py
+++ requests.py
@@ -172,19 +172,13 @@ def render_img2img_grpc(input_file_location, output_file_location, args, depth=F
         init_image=init_img,
         start_schedule=1 - args["init_strength"],
         prompt=prompt_protos,
-        seed=int(seed), # If a seed is provided, the resulting generated image will be deterministic.
-                        # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
-                        # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
-        steps=args["steps"], # Amount of inference steps performed on image generation. Defaults to 30. 
-        cfg_scale=args["cfg_scale"], # Influences how strongly your generation is guided to match your prompt.
-                        # Setting this value higher increases the strength in which it tries to match your prompt.
-                        # Defaults to 7.0 if not specified.
-        width=512, # Generation width, defaults to 512 if not included.
-        height=512, # Generation height, defaults to 512 if not included.
-        samples=1, # Number of images to generate, defaults to 1 if not included.
-        sampler=sampler # Choose which sampler we want to denoise our generation with.
-                                                    # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
-                                                    # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
+        seed=int(seed),
+        steps=args["steps"],
+        cfg_scale=args["cfg_scale"],
+        width=512,
+        height=512,
+        samples=1,
+        sampler=sampler
     )
 
 
